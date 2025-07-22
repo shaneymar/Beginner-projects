1318ccbb5d146172372030ffdaa96fe500964b87
SHA <33160317+shaneymar@users.noreply.github.com>
2025-07-22 19:10:23 +0530

    Initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+
+const Counter = () => {
+
+const[count,setCount] = useState(0);
+
+
+    function handleIncrement(){
+        setCount(count +1);
+    }
+    function handleDecrement(){
+        setCount(count -1);
+    }
+  return (
+    <div className='container'>
+        <div>
+        <h1 className='number'>{count}</h1>
+        </div>
+
+        <div className='btns-container'>
+            <button className='increment' onClick={handleIncrement}>Increment</button>
+            <button className='increment' onClick={handleDecrement}>Decrement</button>
+        </div>
+        
+        
+    </div>
+  )
+}
+
+export default Counter
